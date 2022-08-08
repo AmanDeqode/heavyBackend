650ffba49d2c0560131757dacf0621929870ae75
Aman Chaudhary <achoudhary@deqode.com>
2022-08-08 19:54:51 +0530

    chore: minor fixes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
   });
-  const configService = app.get<ConfigService>(ConfigService);
-  await app.listen(configService.get('app.port'));
+  await app.listen(3000);
 }
 bootstrap();
